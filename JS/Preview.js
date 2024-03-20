12a6fe99083072234f729580f4c82f7db11622ff
sziprohincs <156736638+sziprohincs@users.noreply.github.com>
2024-03-20 13:16:34 +0300

    Add files via upload

---
+++ JS/Preview.js
@@ -0,0 +1,13 @@
+// =====================
+//        LOADING
+// =====================
+
+var timeLimit = 5000;
+window.addEventListener("load", function() {
+	window.location.href = "E:/Users/ASUS/Desktop/AlionCraft/HTML/Index.html";
+});
+
+var errorTimer = setTimeout(function() {
+	document.getElementById("loading").style.display = "none";
+	document.getElementById("error-message").style.display = "block";
+}, timeLimit);
